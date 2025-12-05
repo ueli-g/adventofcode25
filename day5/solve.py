29516994276d460572ebc03c91c4731f0c6594e7
Ueli Graf <ueli@9tlabs.com>
2025-12-05 12:46:22 +0100

    more optimization

--- day5/solve.py
+++ day5/solve.py
@@ -5,7 +5,7 @@ class Crange:
     def __init__(self, start, stop):
         self.start = start
         self.stop = stop
-    def __contains__(self, other):
+    def intersects(self, other):
         return self.start <= other.start <= self.stop
     def __lt__(self, other):
         return self.start < other.start
@@ -16,7 +16,7 @@ class Crange:
         return self
     def __len__(self):
         return self.stop - self.start
-    def has(self, num):
+    def __contains__(self, num):
         return self.start <= num < self.stop
 
 class RangeTree:
@@ -39,42 +39,46 @@ class RangeLeaf:
     def __init__(self, _range):
         self.range = _range
     def __contains__(self, num):
-        return self.range.has(num)
+        return num in self.range
 
 def bisect(lrange, rrange):
     assert(rrange.start > lrange.stop)
     return (lrange.stop + rrange.start)//2
 
 def solve(filename):
-    ranges = set()
-    candidates = set()
+    range_list = list()
+    candidates = list()
     with open(filename) as infile:
         for line in infile.readlines():
             l = line.strip()
             if '-' in l:
                 left, right = l.split('-')
-                ranges.add(Crange(int(left), int(right)+1))
+                range_list.append(Crange(int(left), int(right)+1))
             elif l:
-                candidates.add(int(l))
+                candidates.append(int(l))
 
-    range_list = list(ranges)
     range_list.sort()
     i = 0
     while i < len(range_list)-1:
-        if range_list[i+1] in range_list[i]:
+        if range_list[i].intersects(range_list[i+1]):
             range_list[i] += range_list[i+1]
             del(range_list[i+1])
             continue
         i += 1
 
     node = RangeTree(range_list)
-
     count = sum([c in node for c in candidates])
-    #print(f"{filename}: There are {count} fresh ingredients")
     nitems = sum([len(r) for r in range_list])
-    #print(f"{filename}: There are {nitems} items")
+    return count, nitems
+    
 
 if __name__=='__main__':
     t = timeit.timeit(lambda: solve('test.txt'), number=1000)
     t += timeit.timeit(lambda: solve('input.txt'), number=1000)
-    print(f"took {t}")
+    print(f"took {t}s")
+    count, nitems = solve('test.txt')
+    print(f"test.txt: There are {count} fresh ingredients")
+    print(f"test.txt: There are {nitems} items")
+    count, nitems = solve('input.txt')
+    print(f"input.txt: There are {count} fresh ingredients")
+    print(f"input.txt: There are {nitems} items")
